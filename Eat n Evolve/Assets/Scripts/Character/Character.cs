a5bfa317f8ccb50d6a51de77424a5fe7035576ff
mahyar121 <mahyarhajibabaie@gmail.com>
2019-08-10 13:56:57 -0700

    Made minimap round

---
+++ Eat n Evolve/Assets/Scripts/Character/Character.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+
+    [SerializeField] protected float movementSpeed = 5f;
+
+
+    public abstract void Death();
+    public abstract void Initialize();
+}
--- Eat n Evolve/Assets/Scripts/Player/PlayerController.cs
+++ Eat n Evolve/Assets/Scripts/Player/PlayerController.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : Character
 {
-    [SerializeField] private float movementSpeed = 5f;
-
     // character top down movement
     private Vector3 inputMovement;
     private Vector3 startPosition;
@@ -13,6 +11,10 @@ public class PlayerController : MonoBehaviour
     public Animator MyAnimator { get; set; }
     public Rigidbody2D MyRigidBody2D { get; set; }
     public Transform MyTransform { get; set; }
+
+    public bool HasClaws { get; set; }
+    public bool HasHorns { get; set; }
+    public bool HasSpikes { get; set; }
 
     // Creates a singleton of the Player so we dont make multiple instances of the player
     private static PlayerController instance;
@@ -28,6 +30,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
+        Initialize();
         MyTransform = GetComponent<Transform>();
         MyRigidBody2D = GetComponent<Rigidbody2D>();
     }
@@ -44,6 +47,11 @@ public class PlayerController : MonoBehaviour
         HandleMovement();
     }
 
+    public override void Initialize()
+    {
+        RandomCharacterTraitSelection();
+    }
+
     // Will handle the top down movement of the player
     private void HandleMovement()
     {
@@ -52,7 +60,28 @@ public class PlayerController : MonoBehaviour
         MyRigidBody2D.velocity = new Vector2(horizontal * movementSpeed, vertical * movementSpeed);
     }
 
-  
+    private void HandleInput()
+    {
+        // This is the attack
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // if the player has claws do the claws animation
+            if (Instance.HasClaws)
+            {
+                Debug.Log("Striked with claws!");
+            }
+            // if player has horns do the horns animation
+            if (Instance.HasHorns)
+            {
+                Debug.Log("Striked with horns!");
+            }
+            // if player has spikes do the spikes animation
+            if (Instance.HasSpikes)
+            {
+                Debug.Log("Striked with spikes!");
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -64,9 +93,38 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    private void HandleInput()
+    public override void Death()
     {
-        // This is the attack
-        if (Input.GetKeyDown(KeyCode.Space)) {  }
+        Initialize();
+    }
+
+    // Randomly selects a character trait for the player
+    public void RandomCharacterTraitSelection()
+    {
+        Instance.HasClaws = false;
+        Instance.HasHorns = false;
+        Instance.HasSpikes = false;
+
+        int characterTraitState = Random.Range(1, 4);
+        Debug.Log("Trait Number:" + characterTraitState);
+        switch (characterTraitState) {
+            case 1:
+                Instance.HasClaws = true;
+                Debug.Log("Player got claws!");
+                break;
+            case 2:
+                Instance.HasHorns = true;
+                Debug.Log("Player got horns!");
+                break;
+            case 3:
+                Instance.HasSpikes = true;
+                Debug.Log("Player got spikes!");
+                break;
+            default:
+                Instance.HasClaws = true;
+                Debug.Log("Defaulted the player to claws!");
+                break;
+        }
+      
     }
 }
