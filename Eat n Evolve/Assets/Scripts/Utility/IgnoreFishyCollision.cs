4a436d65bf609e56c630510a5e414760a73ffa8b
Ates Bayraktaroglu <tabayrak@gmail.com>
2019-09-01 13:58:44 -0700

    added a script for fishy but didn't work on it. Added bounding boxes.

---
+++ Eat n Evolve/Assets/Scripts/Utility/IgnoreFishyCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreFishyCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics2D.IgnoreCollision(GetComponent<Collider2D>(), PlayerController.Instance.GetComponent<Collider2D>(), true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
