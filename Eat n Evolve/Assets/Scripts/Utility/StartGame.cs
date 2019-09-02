f6f4f3bf0ef704f89820c37e3938d468f20208a3
Ates Bayraktaroglu <tabayrak@gmail.com>
2019-09-02 16:05:16 -0700

    Added Temp Splash Screen and script to load the TestBed Scene

---
+++ Eat n Evolve/Assets/Scripts/Utility/StartGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class StartGame : MonoBehaviour
+{
+
+    void Start()
+    {
+        Debug.Log("LoadSceneB");
+    }
+
+    public void NextScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("TestBed");
+
+    }
+}
